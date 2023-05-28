7136480ef48e77a9646f672e5119d94e558daa69
Alexei Selivanov <alexei.selivanov@gmail.com>
2023-05-29 02:07:38 +0300

    FAQ page tweaks

--- src/components/CardBanner.jsx
+++ src/components/CardBanner.jsx
@@ -1,9 +1,9 @@
 import classNames from 'classnames'
 import styles from './CardBanner.module.scss'
 
-export default function CardBanner({ className, children, footer }) {
+export default function CardBanner({ className, containerClassName, children, footer }) {
     return (
-        <div className='container-fluid'>
+        <div className={classNames('container-fluid', containerClassName)}>
             <div className={classNames('rounded-5', styles.banner, className)}>
                 <div className='container'>
                     {children}
--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,7 +1,9 @@
-import { useRef, useState, useEffect, useMemo } from 'react'
+import { useRef, useState, useEffect, useMemo, useId } from 'react'
 import classNames from 'classnames'
 import Header from '@/components/Header.jsx'
-import HeroBanner from '@/components/HeroBanner.jsx'
+import CardBanner from '@/components/CardBanner.jsx'
+import lunr from 'lunr'
+
 import QuestionsBanner from '@/components/QuestionsBanner.jsx'
 import NeedMoreHelpForm from '@/components/NeedMoreHelpForm.jsx'
 import Footer from '@/components/Footer.jsx'
@@ -16,6 +18,10 @@ import { ReactComponent as Icon3SVG } from '@/assets/faq-3.svg'
 import { ReactComponent as Icon4SVG } from '@/assets/faq-4.svg'
 import { ReactComponent as Icon5SVG } from '@/assets/faq-5.svg'
 
+import specularImg from '@/assets/specular.png'
+import { ReactComponent as RarrIconSVG } from '@/assets/rarr-icon.svg'
+import { ReactComponent as CrossIconSVG } from '@/assets/cross-icon.svg'
+
 const icons = [
     Icon1SVG,
     Icon2SVG,
@@ -23,6 +29,26 @@ const icons = [
     Icon4SVG,
     Icon5SVG
 ]
+
+let idGenerator = 1;
+const faqMap = {}
+const index = lunr(function () {
+    this.field('question')
+    this.field('answer')
+    this.ref('id')
+
+    for (const [groupAt, group] of faqGroups.entries()) {
+        group.index = groupAt
+        for (const [questionAt, question] of group.questions.entries()) {
+            question.id = idGenerator++
+            question.group = group
+            question.index = questionAt
+            faqMap[question.id] = question
+            this.add(question)
+        }
+    }
+})
+
 
 export default function FAQ() {
     const modalRef = useRef(null)
@@ -34,10 +60,7 @@ export default function FAQ() {
 
     const CurrentGroupIcon = icons[groupAt] || icons[0]
 
-    console.log('FAQ')
-
     useEffect(() => {
-        console.log('useEffect')
         initModal()
 
         async function initModal() {
@@ -50,62 +73,112 @@ export default function FAQ() {
         }
     }, [])
 
+    const [isActive, setIsActive] = useState(false) // TODO remove placeholder
+    const [query, setQuery] = useState('')
+    const [searchResults, setSearchResults] = useState(null)
+
+    function isSearchActive() {
+        return query.length >= 3
+    }
+
+    const suggestionsRef = useRef(null)
+
+    function triggerSearch() {
+        if (!isSearchActive()) return
+
+        const matches = index.search(query).splice(0, 10)
+        setSearchResults(matches.map(({ ref }) => faqMap[ref]))
+    }
+
+    useEffect(() => {
+        triggerSearch()
+    }, [query])
+
     return (
         <>
-            <div className='modal' ref={modalRef} tabIndex={-1}>
-                <div className='modal-dialog modal-xl'>
-                    <div className='modal-content'>
-                        <div className='modal-body p-3 p-md-5'>
-                        {groupAt !== null ? 
-                        <>
-                            <header className='d-flex'>
-                                <CurrentGroupIcon className='flex-shrink-0' />
-                                <div className='flex-grow-1 ms-2 ms-md-4 fw-medium'>
-                                    <div className='small opacity-40'>FAQ /</div>
-                                    <div>{faqGroups[groupAt].title}</div>
-                                </div>
+            <Header />
+            
+            <CardBanner
+                className={classNames(
+                    styles.banner, 
+                    'bg-primary text-light rounded-5 text-center',
+                )}
+                containerClassName={classNames(styles.bannerWrapper, (isActive || isSearchActive()) && styles.active)}
+            >
+                <div className={styles.collapse}>
+                    <div className={classNames(styles.bg, 'rounded-5 user-select-none')}>
+                        <img src={specularImg} className={classNames(styles.specular, styles.left)} />
+                        <img src={specularImg} className={classNames(styles.specular, styles.right)} />
+                    </div>
 
-                                <button 
-                                    type='button' 
-                                    class='btn-close' 
-                                    aria-label='Close'
-                                    onClick={() => modal.hide()}
-                                />
-                            </header>
+                    <h1>How Can We Help You?</h1>
+                    <p className="lead opacity-50 mt-3 mb-4">
+                        Below you’ll find answers to the questions we<br />
+                        get asked the most about the Mailum.
+                    </p>
+                </div>
 
-                            <div className='card mt-3 mt-md-4'>
-                                <div className='card-body'>
-                                    <h4 className='card-title'>{faqGroups[groupAt].questions[questionAt].question}</h4>
-                                    <p className='mb-0'>{faqGroups[groupAt].questions[questionAt].answer}</p>
-                                </div>
-                            </div>
+                <div 
+                    ref={suggestionsRef}
+                    className={classNames(styles.search, 'mx-auto', (isActive || isSearchActive()) && styles.active)}
+                >
+                    <input 
+                        type="text" 
+                        class="form-control bg-primary text-bg-primary" 
+                        placeholder="What can we help you with?" 
+                        onFocus={(e) => {
+                            setIsActive(true)
+                            triggerSearch()
+                        }}
+                        onBlur={() => {
+                            setTimeout(() => setIsActive(false), 100)
+                        }}
+                        value={query}
+                        onChange={(e) => {
+                            setQuery(e.target.value)
+                        }}
+                    />
+                    
+                    <CrossIconSVG className={styles.reset} role='button' onClick={() => {
+                        setQuery('')
+                    }} /> 
 
-                        {otherQuestions.map((question, index) => (
-                            <div className='card mt-2' key={index}>
-                                <div className='card-body py-4'>
-                                    <h6
-                                        className={classNames(styles.cardToggle, 'card-title mb-0 collapsed')}
-                                        data-bs-toggle='collapse'
-                                        data-bs-target={`#q${index}`}
-                                        role='button'
-
-                                    >{question.question}</h6>
-                                    <p
-                                        className='collapse mt-2 mb-0'
-                                        id={`q${index}`}
-                                    >{question.answer}</p>
-                                </div>
-                            </div>
-                        ))}
-                        </>
-                        : null}
+                    {searchResults && 
+                    <div className={classNames(
+                        styles.suggestions, 
+                        'card position-absolute text-start', 
+                        !(isSearchActive()) && styles.hidden
+                    )}>
+                        <div className='card-body'>
+                        {searchResults.length > 0 ?
+                            <ul className={classNames(styles.items, 'list-unstyled m-0')}>
+                            {searchResults.map((question, index) => (
+                                <li 
+                                    key={question.id} 
+                                    className={classNames('small py-3', index > 0 && 'border-top')}
+                                    role='button'
+                                    onClick={() => {
+                                        setGroupAt(question.group.index)
+                                        setQuestionAt(question.index)
+                                        modal.show()
+                                    }}
+                                >
+                                    <p className='text-truncate m-0'>
+                                        <span className='badge'>{question.group.title}</span>
+                                        <span className='content ms-2'>{question.question}</span>
+                                    </p>
+                                </li>
+                            ))}
+                            </ul> :
+                            <p className='small opacity-50 my-3'>
+                                No results.
+                            </p>
+                        }
                         </div>
                     </div>
+                    }
                 </div>
-            </div>
-
-            <Header />
-            <HeroBanner />
+            </CardBanner>
 
             <div className={classNames('container mb-2 mb-md-4', styles.groups)}>
                 <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
@@ -189,6 +262,57 @@ export default function FAQ() {
                             <span className='d-md-none'>Upgrade Now</span>
                             
                         </a>
+                    </div>
+                </div>
+            </div>
+            <div className='modal' ref={modalRef} tabIndex={-1}>
+                <div className='modal-dialog modal-xl'>
+                    <div className='modal-content'>
+                        <div className='modal-body p-3 p-md-5'>
+                        {groupAt !== null ? 
+                        <>
+                            <header className='d-flex'>
+                                <CurrentGroupIcon className='flex-shrink-0' />
+                                <div className='flex-grow-1 ms-2 ms-md-4 fw-medium'>
+                                    <div className='small opacity-40'>FAQ /</div>
+                                    <div>{faqGroups[groupAt].title}</div>
+                                </div>
+
+                                <button 
+                                    type='button' 
+                                    class='btn-close' 
+                                    aria-label='Close'
+                                    onClick={() => modal.hide()}
+                                />
+                            </header>
+
+                            <div className='card mt-3 mt-md-4'>
+                                <div className='card-body'>
+                                    <h4 className='card-title'>{faqGroups[groupAt].questions[questionAt].question}</h4>
+                                    <p className='mb-0'>{faqGroups[groupAt].questions[questionAt].answer}</p>
+                                </div>
+                            </div>
+
+                        {otherQuestions.map((question, index) => (
+                            <div className='card mt-2' key={index}>
+                                <div className='card-body py-4'>
+                                    <h6
+                                        className={classNames(styles.cardToggle, 'card-title mb-0 collapsed')}
+                                        data-bs-toggle='collapse'
+                                        data-bs-target={`#q${index}`}
+                                        role='button'
+
+                                    >{question.question}</h6>
+                                    <p
+                                        className='collapse mt-2 mb-0'
+                                        id={`q${index}`}
+                                    >{question.answer}</p>
+                                </div>
+                            </div>
+                        ))}
+                        </>
+                        : null}
+                        </div>
                     </div>
                 </div>
             </div>

db038c051a6f16606650b7f003777d681768b694
cyberfear <alex@cyberfear.com>
2024-07-25 18:17:08 -0400

    coupons clipboard fix2

--- public/app/src/authorized/settings/rightpanel/coupons.js
+++ public/app/src/authorized/settings/rightpanel/coupons.js
@@ -214,7 +214,7 @@ define([
                     $("body").append($temp);
                     $temp
                         .val(
-                            app.defaults.get("domain") + "/index.html#createUser/" +
+                            app.defaults.get("domain") + "/mailbox/#signup/" +
                                 app.user.get("userPlan")["coupon"]
                         )
                         .select();

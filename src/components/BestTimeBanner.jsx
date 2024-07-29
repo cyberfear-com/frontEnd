c6f4d04a5104171510e24f3eca0d3ee76458db07
cyberfear <alex@cyberfear.com>
2024-07-29 13:40:35 -0400

    Fix aria label and headings

--- src/components/BestTimeBanner.jsx
+++ src/components/BestTimeBanner.jsx
@@ -12,15 +12,15 @@ export default function BestTimeBanner({
             footer={<img src={bestTimeLogosMobileImg} alt="logos of privacy products" className='w-100 mt-4 d-md-none mb-4' />}
         >
             <div className='text-center'>
-                <h4 className='d-none d-md-block'>
+                <h2 className='d-none d-md-block'>
                     <div className='fw-normal' dangerouslySetInnerHTML={{ __html: firstLine }} />
                     <div dangerouslySetInnerHTML={{ __html: secondLine }} />
-                </h4>
+                </h2>
                 
-                <h3 className='d-md-none'>
+                <h2 className='d-md-none'>
                     <div className='h4 fw-normal' dangerouslySetInnerHTML={{ __html: firstLine }} />
                     <div dangerouslySetInnerHTML={{ __html: secondLine }} />
-                </h3>
+                </h2>
             </div>
 
             <img src={bestTimeLogosImg} alt="logos of privacy products" className='w-100 d-none d-md-block mt-5' />
